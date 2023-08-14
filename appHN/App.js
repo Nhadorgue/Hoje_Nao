c53a2c01b955e01995560d5821c1243e35f12d7c
Nhadorgue <gabrielmoura.angelo@gmail.com>
2023-08-14 20:12:04 -0300

    First Commit

--- appHN/App.js
+++ appHN/App.js
@@ -1,9 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import TelaPadrao from './src/componentes/TelaPadrao';
-import Rotas from './src/rotas';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import { useState } from 'react';
+import { styles } from './src/styles';
 
 export default function App() {
-  return <TelaPadrao><Rotas /></TelaPadrao>;
+  
+  const [email, setEmail] = useState(null)
+  const [senha, setSenha] = useState(null)
 
+  const entrar = () =>{
+    console.log("entrou")
+    console.log(email)
+    console.log(senha)
+  }
+
+  return (
+    <View style={styles.container}>
+
+      <Text h1 style = {styles.h1}>Hoje Não!</Text>
+
+      <Input placeholder='Digite seu usuário'
+        color = 'white'
+        leftIcon={{ type: 'font-awesome', name: 'user', color: '#EC86D0' }}
+        onChangeText={value => setEmail(value)}
+        keyboardType='email-address'
+      />
+
+      <Input placeholder="Digite sua senha" 
+        color = 'white'
+        secureTextEntry={true} 
+        leftIcon={{ type: 'font-awesome', name: 'lock', color: '#EC86D0' }}
+        onChangeText={value => setSenha(value)}
+      />
+
+      <Button title="ENTRAR"
+        buttonStyle={styles.botaoPadrao}
+        titleStyle={styles.textoBotoes}
+        containerStyle={styles.containerBotoes}
+        onPress={() => entrar()}
+      />
+
+      <Button title="Cadastre-se"
+        containerStyle={{
+          position: 'absolute',
+          bottom: 20,
+          width: 200,
+          marginHorizontal: 50,
+          marginVertical: 10,
+          justifyContent: 'flex-end',
+        }}
+        type="clear"
+        titleStyle={{ 
+          textDecorationLine: 'underline', 
+          color: '#EC86D0' 
+        }}
+      />
+    </View>
+  );
 }
+
